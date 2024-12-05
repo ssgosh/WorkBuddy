954e3e6d2d4fa9b21c0d7c2f75399b272738a99b
Sabyasachi Ghosh <ssgosh@gmail.com>
2024-12-05 22:30:01 +0530

    Add block page

--- extension/firefox/content.js
+++ extension/firefox/content.js
@@ -1,5 +1,6 @@
 function redirectToBlockPage() {
-  window.location.href = 'https://www.example.com/block-page.html';
+  // window.location.href = 'https://www.example.com/block-page.html';
+  window.location.href = chrome.runtime.getURL('block-page.html');
 }
 
 redirectToBlockPage();
