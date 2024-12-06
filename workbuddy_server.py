b2534325f132e96d80272561078e87b2029de396
Sabyasachi Ghosh <ssgosh@gmail.com>
2024-12-06 11:50:41 +0530

    Flask server

--- workbuddy_server.py
+++ workbuddy_server.py
@@ -1,12 +1,11 @@
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
-
+from flask import Flask, request, jsonify, send_from_directory
 
 def load_prompt(fname):
     with open(fname, "r") as f:
         return "".join(f.readlines())
-
 
 load_dotenv()
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
@@ -19,7 +18,6 @@ generation_config = {
     "response_mime_type": "application/json",
 }
 
-
 system_instruction = load_prompt("./prompts/system_instruction.txt")
 
 model = genai.GenerativeModel(
@@ -28,15 +26,70 @@ model = genai.GenerativeModel(
     system_instruction=system_instruction,
 )
 
+app = Flask(__name__)
 
-chat_session = model.start_chat()
-while True:
-    # Get the multiline input
-    msg = '''
-        {
-            "page_title" : "Download or install libraries to access Gemini",
-            "page_url" : "https://ai.google.dev/gemini-api/docs/downloads",
-        }
-    '''
-    response = chat_session.send_message(msg)
-    print(response.text)
+@app.route('/')
+def serve_html():
+    return send_from_directory('.', 'hello_gemini.html')
+
+@app.route('/classify', methods=['POST'])
+def classify_page():
+    data = request.json
+    prompt = data.get('prompt')
+    # system_prompt = data.get('system_prompt')
+
+    chat_session = model.start_chat()
+    response = chat_session.send_message(prompt)
+    
+    result = {"text": response.text}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+# import os
+# import google.generativeai as genai
+# from dotenv import load_dotenv
+
+
+# def load_prompt(fname):
+#     with open(fname, "r") as f:
+#         return "".join(f.readlines())
+
+
+# load_dotenv()
+# genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# generation_config = {
+#     "temperature": 1,
+#     "top_p": 0.95,
+#     "top_k": 40,
+#     "max_output_tokens": 8192,
+#     "response_mime_type": "application/json",
+# }
+
+
+# system_instruction = load_prompt("./prompts/system_instruction.txt")
+
+# model = genai.GenerativeModel(
+#     model_name="gemini-1.5-flash",
+#     generation_config=generation_config,
+#     system_instruction=system_instruction,
+# )
+
+
+# chat_session = model.start_chat()
+# while True:
+#     # Get the multiline input
+#     msg = '''
+#         {
+#             "page_title" : "Download or install libraries to access Gemini",
+#             "page_url" : "https://ai.google.dev/gemini-api/docs/downloads",
+#         }
+#     '''
+#     response = chat_session.send_message(msg)
+#     print(response.text)
