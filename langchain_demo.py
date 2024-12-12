9c5dd4bca04d0d2faa03493ee579abc5fc73ae6d
Sabyasachi Ghosh <ssgosh@gmail.com>
2024-12-12 18:30:05 +0530

    Use llama3.2:3b w/ langchain for page classification

--- langchain_demo.py
+++ langchain_demo.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama import ChatOllama
 
 llm = ChatOllama(
-    model="llama3.2:1b",
+    model="llama3.2:3b",
     temperature=0,
     #     "temperature": 1,
     # "top_p": 0.95,
@@ -11,8 +11,30 @@ llm = ChatOllama(
     # "max_output_tokens": 8192,
     top_p=0.95,
     top_k=40,
-    # format="json",
+    format="json",
 )
+
+
+def load_prompt(fname):
+    with open(fname, "r") as f:
+        return "".join(f.readlines())
+
+
+system_instruction = load_prompt("./prompts/system_instruction.txt")
+for msg in [
+    ("human", '{"page_title" : "ChatOllama - LangChain Documentation",'
+     ' "page_url" : "https://python.langchain.com/v0.2/api_reference/'
+     'ollama/chat_models/langchain_ollama.chat_models.ChatOllama.html"}'),
+    ("human", '{"page_title" : "IND vs AUS | Live from Brisbane: '
+     'Rohit\'s position still undecided? | AUS practice updates", '
+     '"page_url" : "https://www.youtube.com/watch?v=bebfsoL9viY"}'),
+     ("human", '{"page_title" : "model temperature llm - Google Search"}')]:
+
+    messages = [
+        ("system", system_instruction), msg
+    ]
+    output = llm.invoke(messages)
+    print(output)
 
 # prompt = ChatPromptTemplate.from_messages(
 #     [
@@ -33,10 +55,10 @@ llm = ChatOllama(
 #     }
 # )
 
-messages = [
-    ("system", "You are a helpful translator. Translate the user sentence to French."),
-    ("human", "I love programming."),
-]
-output = llm.invoke(messages)
+# messages = [
+#     ("system", "You are a helpful translator. Translate the user sentence to French."),
+#     ("human", "I love programming."),
+# ]
+# output = llm.invoke(messages)
 
-print(output)
+# print(output)
